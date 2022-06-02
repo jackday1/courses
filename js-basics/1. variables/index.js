b1577e6f25b0a05cc6c739c638ffd4ad6b6b43fa
jacknguyen <jack.nguyen@weareday.one>
2022-06-02 21:53:16 +0700

    feat(js-basics): 1. variables

---
+++ js-basics/1. variables/index.js
@@ -0,0 +1,12 @@
+// there are 2 ways to define variables
+// use let when you want to change value of variable later
+// use const for the rest
+
+const number = 2;
+// number = 4;
+
+let string = "hello world!";
+string = "hello world 22";
+
+console.log(number);
+console.log(string);
