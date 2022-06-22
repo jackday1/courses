e1b9e8be9ec03fc0d8e45ae318c3f60d929c9ee8
jackday1 <jack.nguyen@weareday.one>
2022-06-22 20:26:53 +0700

    feat(js-basics): implement sort methods

--- @js-basics/7. array methods/advanced.js
+++ @js-basics/7. array methods/advanced.js
@@ -1,17 +1,22 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// map - run a function for all item in array
-const doubleNumbers = numbers.map(number => number * 2)
-console.log(doubleNumbers)
+// map - run a function for all item in array, return a new array that is modified
+const doubleNumbers = numbers.map((number) => number * 2);
+console.log(doubleNumbers);
 
 // find - return the first item that makes function return true
-const evenNumber = numbers.find(number => number % 2 === 0)
-console.log(evenNumber)
+const evenNumber = numbers.find((number) => number % 2 === 0);
+console.log(evenNumber);
 
 // filter - return an array includes all item that make function return true
-const allEvenNumbers = numbers.filter(item => item % 2 === 0)
-console.log(allEvenNumbers)
+const allEvenNumbers = numbers.filter((item) => item % 2 === 0);
+console.log(allEvenNumbers);
+
+// sort - sort array
+const strings = ["a", "g", "b", "d"];
+strings.sort();
+console.log(strings);
 
 // reduce - loop through every item in array
-const sum = numbers.reduce((total, number) => total + number, 0)
-console.log(sum)
+const sum = numbers.reduce((total, number) => total + number, 0);
+console.log(sum);
