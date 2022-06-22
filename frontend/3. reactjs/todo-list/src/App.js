f60489fc626fe09887d797bdf271e58940d97bb3
jackday1 <jack.nguyen@weareday.one>
2022-06-22 15:07:46 +0700

    feat(frontend/reactjs): todo list create function

--- frontend/3. reactjs/todo-list/src/App.js
+++ frontend/3. reactjs/todo-list/src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 import "./App.css";
-import { get, update, remove } from "./services/todo.service";
+import { create, get, update, remove } from "./services/todo.service";
 
 import Loading from "./components/Loading";
 import TodoItem from "./components/TodoItem";
@@ -9,13 +9,42 @@ import TodoItem from "./components/TodoItem";
 const App = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
   const getData = async () => {
     setIsLoading(true);
 
     try {
       const res = await get();
-      setTodos(res.data);
+      setTodos(
+        res.data.sort((todo1, todo2) => todo2.createdAt - todo1.createdAt)
+      );
+    } catch (err) {
+      console.error(err);
+    }
+
+    setIsLoading(false);
+  };
+
+  const addTodo = async () => {
+    if (!title || !title.trim() || !description || !description.trim()) {
+      alert("Please fill in title and description");
+      return;
+    }
+
+    setIsLoading(true);
+
+    try {
+      await create({
+        title,
+        description,
+        isDone: false,
+        createdAt: Date.now(),
+      });
+      setTitle("");
+      setDescription("");
+      await getData();
     } catch (err) {
       console.error(err);
     }
@@ -60,6 +89,23 @@ const App = () => {
     <div className="App">
       <Loading isLoading={isLoading} />
       <h3>TODO LIST</h3>
+      <div className="todo-create">
+        <input
+          className="todo-input"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          className="todo-input"
+          placeholder="Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button className="create-todo-btn" onClick={addTodo}>
+          Create todo
+        </button>
+      </div>
       <div className="todo-list">
         {todos.map((todo) => (
           <TodoItem
