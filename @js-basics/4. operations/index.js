66e947848a0eedfe5c625d0ec017a689690803de
jackday1 <jack.nguyen@weareday.one>
2022-07-06 20:39:41 +0700

    feat(js-basics): add operation lesson

---
+++ @js-basics/4. operations/index.js
@@ -0,0 +1,34 @@
+// + - * %
+
+// += -= *= %=
+let number = 3;
+number += 3; // number = number + 3
+console.log(number);
+
+// > <
+
+// == ===
+const num = 2;
+const string = "2";
+
+console.log(num == string);
+console.log(num === string);
+
+// != !==
+console.log(num != string);
+console.log(num !== string);
+
+// && ||
+const number1 = 2;
+const number2 = 3;
+console.log(number1 === 2 || number2 === 4);
+console.log(number1 === 2 && number2 === 4);
+
+// ++ --
+let i = 1;
+console.log(i++);
+console.log(i);
+
+let j = 1;
+console.log(++j);
+console.log(j);
