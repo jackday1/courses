9272c6f62d224e50e26a3ca600cb0874d69f0d5b
jackday1 <jack.nguyen@weareday.one>
2022-07-10 22:27:12 +0700

    chores(@js-basics): fix array

--- @js-basics/7. array/index.js
+++ @js-basics/7. array/index.js
@@ -1,31 +1,29 @@
 // length of an array
-const numbers = [1, 2, 3, 4]
-console.log(numbers.length)
+const numbers = [1, 2, 3, 4];
+console.log(numbers.length);
 
 // get an item in an array
-console.log(numbers[0])
-console.log(numbers[numbers.length - 1])
+console.log(numbers[0]);
+console.log(numbers[numbers.length - 1]);
 
 // loop through array items
-for (const item of array) {
-  console.log(item)
+for (const item of numbers) {
+  console.log(item);
 }
 
 // overwrite an item in array
-numbers[1] = 6
-console.log(numbers)
+numbers[1] = 6;
+console.log(numbers);
 
-numbers[4] = 7
-console.log(numbers)
-console.log(numbers.length)
+numbers[4] = 7;
+console.log(numbers);
+console.log(numbers.length);
 
 // Practice: Write a function has an array as parameter:
 // - if the length of array < 5, add 1 to the array until its length = 5 and return this array after modification
 // - if the length of array = 5 return string "Array length is big enough"
 
-const updateArray = array => {
+const updateArray = (array) => {};
 
-}
-
-updateArray([1, 2, 3]) // --> expect: [1, 2, 3, 1, 1]
-updateArray([1, 2, 3, 4, 5]) // --> expect: Array length is big enough
+updateArray([1, 2, 3]); // --> expect: [1, 2, 3, 1, 1]
+updateArray([1, 2, 3, 4, 5]); // --> expect: Array length is big enough
